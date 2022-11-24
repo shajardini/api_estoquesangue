80fbcef6d15c02f55cb1d5a17bfe86da632eaee6
shajardini <sha.jardini@gmail.com>
2022-11-24 12:22:33 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
+const cors = require('cors')
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
+
 //Rotas
 const index = require('./routes/index');
 const personRoute = require('./routes/personRoute');
